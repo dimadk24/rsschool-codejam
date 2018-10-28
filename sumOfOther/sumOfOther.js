f5210684df1681e2b605d9ca301b37a578e2211c
DimaDK <DimaKrutDK@ya.ru>
2018-10-28 11:44:19 +0300

    feat: sumOfOther function with awesome tests

--- sumOfOther/sumOfOther.js
+++ sumOfOther/sumOfOther.js
@@ -1,3 +1,21 @@
-module.exports = function sumOfOther(arr) {
+function getArraySum(arr) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr.reduce(reducer);
+}
 
+function getOtherItems(arr, index) {
+  return arr.filter((item, i) => i !== index);
+}
+
+function getSumOfOtherForItem(arr, index) {
+  const otherItems = getOtherItems(arr, index);
+  return getArraySum(otherItems);
+}
+
+function sumOfOther(arr) {
+  return arr.map((item, index) => getSumOfOtherForItem(arr, index));
+}
+
+module.exports = {
+  sumOfOther, getSumOfOtherForItem, getOtherItems, getArraySum,
 };
--- sumOfOther/sumOfOther.test.js
+++ sumOfOther/sumOfOther.test.js
@@ -1,12 +1,61 @@
 const assert = require('assert');
-const sumOfOther = require('./sumOfOther');
+const {
+  sumOfOther, getSumOfOtherForItem, getOtherItems, getArraySum,
+} = require('./sumOfOther');
 
 describe('sumOfOther', function () {
-  it('1', function () {
+  it('should sum basic numbers', function () {
     assert.deepEqual(sumOfOther([2, 3, 4, 1]), [8, 7, 6, 9]);
   });
-
-  it('2', function () {
+  it('should sum another basic numbers', function () {
     assert.deepEqual(sumOfOther([7, 5, 2, 8]), [15, 17, 20, 14]);
+  });
+  it('should sum negative numbers', function () {
+    assert.deepEqual(sumOfOther([-5, -7, -2, -4]), [-13, -11, -16, -14]);
+  });
+});
+
+describe('getSumOfOtherForItem', function () {
+  it('1', function () {
+    assert.deepEqual(getSumOfOtherForItem([2, 3, 4, 1], 0), 8);
+  });
+  it('2', function () {
+    assert.deepEqual(getSumOfOtherForItem([2, 3, 4, 1], 1), 7);
+  });
+  it('3', function () {
+    assert.deepEqual(getSumOfOtherForItem([2, 3, 4, 1], 2), 6);
+  });
+  it('4', function () {
+    assert.deepEqual(getSumOfOtherForItem([2, 3, 4, 1], 3), 9);
+  });
+});
+
+describe('getOtherItems', function () {
+  it('1', function () {
+    assert.deepEqual(getOtherItems([2, 3, 4, 1], 0), [3, 4, 1]);
+  });
+  it('2', function () {
+    assert.deepEqual(getOtherItems([2, 3, 4, 1], 1), [2, 4, 1]);
+  });
+  it('3', function () {
+    assert.deepEqual(getOtherItems([2, 3, 4, 1], 2), [2, 3, 1]);
+  });
+  it('4', function () {
+    assert.deepEqual(getOtherItems([2, 3, 4, 1], 3), [2, 3, 4]);
+  });
+});
+
+describe('getArraySum', function () {
+  it('1', function () {
+    assert.equal(getArraySum([2, 3, 4, 1]), 10);
+  });
+  it('2', function () {
+    assert.equal(getArraySum([2, 4, 1]), 7);
+  });
+  it('3', function () {
+    assert.equal(getArraySum([2, 3, 1]), 6);
+  });
+  it('4', function () {
+    assert.equal(getArraySum([2, 3, 4]), 9);
   });
 });
